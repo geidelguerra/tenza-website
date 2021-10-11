06247420ae1b1948907b6d98d05091f0542f17df
Geidel Guerra <geidelguerra@protonmail.com>
2021-10-10 20:03:50 -0400

    Add comming soon landing

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'tenza-website',
+    title: 'Tenza Design',
     htmlAttrs: {
       lang: 'en'
     },
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <Tutorial />
+  <div class="flex justify-center items-center min-h-screen bg-black">
+    <div>
+      <h1 class="font-bold text-4xl text-center text-white">
+        Tenza Design
+      </h1>
+      <div class="text-center text-green-400">
+        Comming soon
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
