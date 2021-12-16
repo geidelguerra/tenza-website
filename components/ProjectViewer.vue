bf4fd2ff97de56f5aab117c12eec2f7610292192
Geidel Guerra <geidelguerra@protonmail.com>
2021-12-16 13:25:21 -0500

    WIP

--- components/ProjectViewer.vue
+++ components/ProjectViewer.vue
@@ -4,9 +4,9 @@
     <div v-if="showDebugStats" class="absolute top-2 right-2 z-40 font-mono">
       <div v-if="cameraStats" class="flex justify-end flex-col text-right">
         <pre>
-          <span class="font-bold">       Distance:</span> {{ cameraStats.distance.toFixed(2) }}
-          <span class="font-bold">    Polar Angle:</span> {{ cameraStats.polarAngle.toFixed(2) }}
-          <span class="font-bold">Azimuthal Angle:</span> {{ cameraStats.azimuthalAngle.toFixed(2) }}
+          <span class="font-bold">       Distance:</span> {{ cameraStats.distance.toFixed(2) }}m
+          <span class="font-bold">    Polar Angle:</span> {{ cameraStats.polarAngle.toFixed(2) }}&deg;
+          <span class="font-bold">Azimuthal Angle:</span> {{ cameraStats.azimuthalAngle.toFixed(2) }}&deg;
         </pre>
       </div>
     </div>
@@ -29,11 +29,21 @@ export default {
   },
   mounted () {
     const projectScene = this.projectScene = new ProjectScene(this.$refs.canvas, {
-      onCameraChange: (stats) => { this.cameraStats = stats }
+      onCameraChange: (stats) => { this.cameraStats = stats },
+      castShadow: false,
+      showHelpers: process.env.debug,
+      showStats: process.env.debug
     })
+
     projectScene.init(this.$el.clientWidth, this.$el.clientHeight)
     projectScene.loadModel(this.project.scene.path)
     projectScene.loadEnvHDR('/hdr/urban_street_01_2k.hdr')
+
+    window.addEventListener('keyup', (event) => {
+      if (event.key === 'r') {
+        this.projectScene.resetCamera()
+      }
+    })
 
     window.addEventListener('resize', () => {
       projectScene.setSize(this.$el.clientWidth, this.$el.clientHeight)
--- threejs/ProjectScene.js
+++ threejs/ProjectScene.js
@@ -6,7 +6,10 @@ import {
   sRGBEncoding,
   PMREMGenerator,
   AnimationMixer,
-  UnsignedByteType
+  UnsignedByteType,
+  PCFSoftShadowMap,
+  AxesHelper,
+  MathUtils
 } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
@@ -32,14 +35,26 @@ export class ProjectScene {
     this.envHrdLoader = null
     this.mixers = []
     this.clock = new Clock()
+    this.stats = null
     this.onCameraChange = options.onCameraChange || noop
+    this.castShadow = options.castShadow || false
+    this.showHelpers = options.showHelpers || false
+    this.showStats = options.showStats || false
   }
 
   init (width, height) {
-    this.renderer = new WebGLRenderer({ canvas: this.canvas, alpha: true })
+    this.renderer = new WebGLRenderer({ canvas: this.canvas, alpha: true, antialias: true })
+    this.renderer.physicallyCorrectLights = true
+    this.renderer.outputEncoding = sRGBEncoding
+    // this.renderer.toneMapping = ReinhardToneMapping
+    // this.renderer.toneMappingExposure = 1
     this.renderer.setSize(width, height)
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-    this.renderer.outputEncoding = sRGBEncoding
+
+    if (this.castShadow) {
+      this.renderer.shadowMap.enabled = true
+      this.renderer.shadowMap.type = PCFSoftShadowMap
+    }
 
     this.pmremGenerator = new PMREMGenerator(this.renderer)
     this.pmremGenerator.compileEquirectangularShader()
@@ -48,25 +63,43 @@ export class ProjectScene {
     // this.scene.background = new Color(0xBFE3DD)
     // this.scene.environment = this.pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture
 
-    this.camera = new PerspectiveCamera(75, width / height, 0.1, 1000)
-    this.camera.position.set(5, 2, 8)
+    this.camera = new PerspectiveCamera(30, width / height, 0.1, 100)
+    this.camera.position.set(63, 63, 77)
     this.scene.add(this.camera)
 
     this.loader = new GLTFLoader()
     this.loader.setDRACOLoader(dracoLoader)
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-    this.controls.target.set(0, 0.5, 0)
+    this.controls.target.set(0, 0, 0)
     this.controls.enablePan = true
+    this.controls.screenSpacePanning = true
     this.controls.enableDamping = true
+    this.controls.dampingFactor = 0.025
+    this.controls.minDistance = 40
+    this.controls.maxDistance = 77
+    this.controls.minPolarAngle = MathUtils.degToRad(40)
+    this.controls.maxPolarAngle = MathUtils.degToRad(90)
+    this.controls.saveState()
     this.controls.update()
     this.controls.addEventListener('change', () => {
       this.onCameraChange({
         distance: this.controls.getDistance(),
-        polarAngle: this.controls.getPolarAngle(),
-        azimuthalAngle: this.controls.getAzimuthalAngle()
+        polarAngle: MathUtils.radToDeg(this.controls.getPolarAngle()),
+        azimuthalAngle: MathUtils.radToDeg(this.controls.getAzimuthalAngle())
       })
     })
+
+    this.onCameraChange({
+      distance: this.controls.getDistance(),
+      polarAngle: MathUtils.radToDeg(this.controls.getPolarAngle()),
+      azimuthalAngle: MathUtils.radToDeg(this.controls.getAzimuthalAngle())
+    })
+
+    if (this.showHelpers) {
+      const axesHelper = new AxesHelper(50)
+      this.scene.add(axesHelper)
+    }
 
     const tick = () => {
       requestAnimationFrame(tick)
@@ -79,6 +112,10 @@ export class ProjectScene {
     }
 
     tick()
+  }
+
+  resetCamera () {
+    this.controls.reset()
   }
 
   setSize (width, height) {
@@ -109,7 +146,12 @@ export class ProjectScene {
 
     return new Promise((resolve, reject) => {
       this.loader.load(url, (gltf) => {
+        // eslint-disable-next-line no-console
+        console.log('Scene:', gltf.scene)
+
         const model = gltf.scene
+        model.castShadow = this.castShadow
+
         this.scene.add(model)
 
         if (gltf.animations.length > 0) {
