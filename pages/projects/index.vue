30a268fc8dd87a04d6237a332d98df8afc0b3b11
Geidel Guerra <geidelguerra@protonmail.com>
2022-06-21 14:34:03 -0400

    WIP

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,6 +1,41 @@
 <template>
-  <div class="relative">
-    <div class="absolute top-[224px] bottom-[133px] w-full overflow-hidden flex flex-col justify-center">
+  <div class="relative w-screen h-screen">
+    <nuxt-link :to="`/projects/${activeProject.id}`" class="absolute top-0 left-0 block w-full h-full">
+      <img :src="activeProject.thumbnail.url" class="absolute top-0 left-0 object-cover w-full h-full">
+      <div class="absolute top-0 bottom-0 left-0 right-0 bg-gradient-radial from-white to-black mix-blend-multiply opacity-80" />
+    </nuxt-link>
+
+    <div class="absolute top-0 bottom-0 left-[124px] z-30 flex items-center">
+      <button
+        class="text-white transition-opacity duration-200"
+        @click="prevProject"
+      >
+        <ArrowLeft class="w-[27px]" />
+      </button>
+    </div>
+
+    <div class="absolute top-0 bottom-0 right-[124px] z-30 flex items-center">
+      <button
+        class="text-white transition-opacity duration-200"
+        @click="nextProject"
+      >
+        <ArrowRight class="w-[27px]" />
+      </button>
+    </div>
+
+    <div class="absolute bottom-[133px] left-[200px] z-30">
+      <nuxt-link
+        :to="`/projects/${activeProject.id}`"
+        class="block text-[14px] font-semibold uppercase tracking-[7px] text-white hover:underline underline-offset-1"
+        v-html="activeProject.name.replace(' ', '<br>')"
+      />
+    </div>
+    <div class="absolute bottom-[133px] right-[200px] z-30">
+      <div class="text-[14px] font-semibold uppercase tracking-[7px] text-white">
+        {{ (activeProjectIndex + 1).toString().padStart(2, '0') }}/{{ projects.length.toString().padStart(2, '0') }}
+      </div>
+    </div>
+    <!-- <div class="absolute top-[224px] bottom-[133px] w-full overflow-hidden flex flex-col justify-center">
       <div class="relative flex justify-center w-full h-full">
         <div class="flex items-center justify-end pr-[64px] w-[201px] shrink-0">
           <button
@@ -44,7 +79,7 @@
           {{ (activeSlideIndex + 1).toString().padStart(2, '0') }}/{{ numberOfProjects.toString().padStart(2, '0') }}
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -52,13 +87,13 @@
 import { getProjects } from '@/api'
 import ArrowLeft from '~/assets/images/arrow_left.svg?inline'
 import ArrowRight from '~/assets/images/arrow_right.svg?inline'
-import Slider from '~/components/Slider.vue'
+// import Slider from '~/components/Slider.vue'
 
 export default {
   components: {
     ArrowLeft,
-    ArrowRight,
-    Slider
+    ArrowRight
+    // Slider
   },
   async asyncData ({ store }) {
     return {
@@ -67,31 +102,34 @@ export default {
   },
   data () {
     return {
+      activeProjectIndex: 0,
       activeSlideIndex: 0,
       projects: []
     }
   },
   computed: {
-    hasPrevProjects () {
-      return this.activeSlideIndex - 1 > -1
-    },
-    hasNextProjects () {
-      return this.activeSlideIndex + 1 - this.numberOfProjects
-    },
     activeProject () {
-      if (this.activeSlideIndex >= 0 && this.activeSlideIndex < this.numberOfProjects) {
-        return this.projects[this.activeSlideIndex]
-      }
-
-      return null
-    },
-    numberOfProjects () {
-      return this.projects.length
+      return this.projects[this.activeProjectIndex]
     }
   },
   mounted () {
-    this.$store.commit('lightMode', true)
-    this.$store.commit('showFooter', false)
+    this.$store.commit('lightMode', false)
+  },
+  methods: {
+    prevProject () {
+      if (this.activeProjectIndex - 1 >= 0) {
+        this.activeProjectIndex--
+      } else {
+        this.activeProjectIndex = this.projects.length - 1
+      }
+    },
+    nextProject () {
+      if (this.activeProjectIndex + 1 < this.projects.length) {
+        this.activeProjectIndex++
+      } else {
+        this.activeProjectIndex = 0
+      }
+    }
   }
 }
 </script>
