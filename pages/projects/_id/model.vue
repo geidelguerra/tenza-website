4945f3b35ec63969ec8e73e8819f5f3c19a5bbda
Geidel Guerra <geidelguerra@protonmail.com>
2022-06-23 11:01:05 -0400

    WIP

--- pages/projects/_id/model.vue
+++ pages/projects/_id/model.vue
@@ -1,19 +1,35 @@
 <template>
   <div class="relative h-screen bg-[#EBEAE5] pt-[108px]">
+    <client-only>
+      <ThreeDViewer
+        :model-url="project.scene.path"
+        class="absolute top-0 bottom-0 left-0 right-0"
+        @model:loadingStarted="loadingModel = true"
+        @model:loadingProgress="({ progress }) => modelLoadProgress = progress"
+        @model:loadingFinished="loadingModel = false"
+      />
+    </client-only>
     <div v-if="loadingModel" class="fixed top-0 bottom-0 left-0 right-0 z-40 flex items-center justify-center bg-[#EBEAE5]">
       <div>Loading... ({{ modelLoadProgress.toFixed(2) }}%)</div>
     </div>
     <template v-else>
       <div class="fixed bottom-[46px] left-[46px] pointer-events-none z-30">
-        <div class="flex flex-col space-y-[3rem] uppercase text-[11px]">
-          <div class="flex space-x-[2.25rem]">
-            <div><PanIcon /></div> <div>Left click to Pan</div>
+        <div class="flex flex-col justify-center uppercase text-[11px]">
+          <div class="flex items-center">
+            <div class="w-[32px] flex justify-center">
+              <RotateIcon />
+            </div>
+            <div class="ml-[23px]">
+              Drag left click to rotate
+            </div>
           </div>
-          <div class="flex space-x-[2.25rem]">
-            <div><RotateIcon /></div> <div>Middle click to Rotate</div>
-          </div>
-          <div class="flex space-x-[2.25rem]">
-            <div><ZoomIcon /></div> <div>Scroll to Zoom</div>
+          <div class="flex items-center mt-[14px]">
+            <div class="w-[32px] flex justify-center">
+              <ZoomIcon />
+            </div>
+            <div class="ml-[23px]">
+              Scroll to Zoom
+            </div>
           </div>
         </div>
       </div>
@@ -27,15 +43,6 @@
         </NuxtLink>
       </div>
     </template>
-    <client-only>
-      <ThreeDViewer
-        :model-url="project.scene.path"
-        class="absolute top-0 bottom-0 left-0 right-0"
-        @model:loadingStarted="loadingModel = true"
-        @model:loadingProgress="({ progress }) => modelLoadProgress = progress"
-        @model:loadingFinished="loadingModel = false"
-      />
-    </client-only>
   </div>
 </template>
 
@@ -43,7 +50,6 @@
 import ThreeDViewer from '~/components/ThreeDViewer.vue'
 import ArrowLeft from '~/assets/images/arrow_left.svg?inline'
 import ZoomIcon from '~/assets/images/zoom_icon.svg?inline'
-import PanIcon from '~/assets/images/pan_icon.svg?inline'
 import RotateIcon from '~/assets/images/rotate_icon.svg?inline'
 import { getProject } from '@/api'
 
@@ -52,7 +58,6 @@ export default {
     ThreeDViewer,
     ArrowLeft,
     ZoomIcon,
-    PanIcon,
     RotateIcon
   },
   layout: 'project',
