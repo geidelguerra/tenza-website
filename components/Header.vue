1549168c0f7f5d91f78db0a39ac5ca5628960763
Geidel Guerra <geidelguerra@protonmail.com>
2022-08-06 09:23:22 -0400

    WIP

--- components/Header.vue
+++ components/Header.vue
@@ -13,31 +13,38 @@
     >
       <MenuExpanded v-if="menuOpen" class="text-black" />
       <MenuCollapsed
-        v-else
+        v-if="!menuOpen"
         class="transition-colors duration-500"
         :class="lightMode ? 'text-black' : 'text-white'"
       />
     </button>
 
-    <div v-if="menuOpen" class="absolute top-0 pt-[142px] left-0 w-screen h-screen bg-[#f7f7f7] flex flex-col lg:hidden">
-      <ul class="flex flex-col justify-center flex-1">
-        <template v-for="(item, i) in menu">
-          <li :key="i" :class="{'mt-[93px]': i > 0}">
-            <NuxtLink
-              :to="item.url"
-              active-class="font-bold"
-              exact-active-class="font-bold"
-              class="transition-colors duration-500 text-center uppercase block text-[15px] tracking-[7.5px] text-black"
-            >
-              {{ item.text }}
-            </NuxtLink>
-          </li>
-        </template>
-      </ul>
-      <div class="flex justify-center w-full p-[32px]">
-        <Networks :light-mode="true" />
+    <transition
+      enter-active-class="transition-all duration-500"
+      leave-active-class="transition-all duration-500"
+      enter-class="opacity-0"
+      leave-to-class="opacity-0"
+    >
+      <div v-if="menuOpen" class="absolute  top-0 pt-[142px] left-0 w-screen h-screen bg-[#f7f7f7] flex flex-col lg:hidden">
+        <ul class="flex flex-col justify-center flex-1">
+          <template v-for="(item, i) in menu">
+            <li :key="i" :class="{'mt-[93px]': i > 0}">
+              <NuxtLink
+                :to="item.url"
+                active-class="font-bold"
+                exact-active-class="font-bold"
+                class="transition-colors duration-500 text-center uppercase block text-[15px] tracking-[7.5px] text-black"
+              >
+                {{ item.text }}
+              </NuxtLink>
+            </li>
+          </template>
+        </ul>
+        <div class="flex justify-center w-full p-[32px]">
+          <Networks :light-mode="true" />
+        </div>
       </div>
-    </div>
+    </transition>
 
     <ul class="lg:absolute lg:top-[45px] lg:right-[45px] flex-col hidden lg:flex">
       <template v-for="(item, i) in menu">
